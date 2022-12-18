4500666db8ed6d71f6252db9e7cceb3f4c07c2a5
jdustindev <jdustindev@gmail.com>
2022-12-17 20:26:43 -0500

    Reimplement/fix stock_picker() logic
    I basically rewrote the function from scratch, but based parts of the
    new code on the working parts of what was written previously.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,33 +1,26 @@
 def stock_picker(stock_prices)
-    # for each number, get the index max price day after it, and
-    # the difference between that day and the current day
-    # store these in a hash ({0 => {index: ni}, 1 => 4, ...})
-
-    best_days = stock_prices.map.with_index do |price, index|
-        best_day = stock_prices[index..-1].reduce({}) do |max_return, current_price|
-            price_diff = current_price - price
-            if max_return.empty?
-                max_return = {price: current_price, diff: price_diff}
-            end
-            if (price_diff > max_return[:diff])
-                {price: current_price, diff: price_diff}
+    stock_info = stock_prices.map.with_index do |price, index|
+        {day: index, price: price}
+    end
+    all_returns = stock_info.map.with_index do |price, index|
+        stock_info[index..-1].reduce({}) do |max_return, current_day|
+            price_diff = current_day[:price] - stock_info[index][:price]
+            return_info = {buy_day: stock_info[index][:day], sell_day: current_day[:day], price_diff: price_diff}
+            if (max_return.empty? || return_info[:price_diff] > max_return[:price_diff])
+                return_info
             else
                 max_return
             end
         end
-        best_day
     end
-    best_days = best_days.map.with_index do |value, index|
-        value[:buy_day] = index
-        value
-    end
-    best_days.reduce do |best_max_return, current_return|
-        if (current_return[:diff] > best_max_return[:diff])
-            current_return
+    best_day = all_returns.reduce do |max_return, current_day|
+        if (max_return.empty? || current_day[:price_diff] > max_return[:price_diff])
+            current_day
         else
-            best_max_return
+            max_return
         end
     end
+    [best_day[:buy_day], best_day[:sell_day]]
 end
 
-puts stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,1,10])
